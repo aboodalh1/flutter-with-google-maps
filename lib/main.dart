8745fa2f5bce98bc6e876ca4baef20c96b2390aa
aboodalh1 <am8027833@gmail.com>
2024-03-24 23:30:14 +0300

    maps

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,282 @@
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late CameraPosition initialCameraPosition;
+  @override
+  void initState() {
+    initialCameraPosition = const CameraPosition(
+        zoom: 10, target: LatLng(31.187084851056554, 29.928110526889437));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          backgroundColor: Colors.pink[200],
+          title: const ListTile(
+            title: Text(
+              "سفريات العبود",
+              style: TextStyle(fontSize: 18),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+            subtitle: Text("جديدنا! سافر مع كلبك"),
+          ),
         ),
+        body: Stack(children: [
+          GoogleMap(
+              cameraTargetBounds: CameraTargetBounds(LatLngBounds(
+                  northeast: const LatLng(31.187084851056554, 29.928110526889437),
+                  southwest: const LatLng(31.187084851056554, 29.928110526889437))),
+              initialCameraPosition: const CameraPosition(target: LatLng(0, 0))),
+          Positioned(
+              bottom: 16,
+              left: 16,
+              right: 16,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text("Change location"),
+              )),
+        ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+// world view 0->3
+// country view 4->6
+//city view 10->12
+// street view 13->17
+// building view 18->20
+// class pointsCounter extends StatefulWidget {
+//   @override
+//   State<pointsCounter> createState() => _pointsCounterState();
+// }
+
+// class _pointsCounterState extends State<pointsCounter> {
+//   int teamAPoints = 0;
+
+//   int teamBPoints = 0;
+
+//   void addOnePoint() {
+//     print('add one point');
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.orange,
+//           title: const Text('Points Counter'),
+//         ),
+//         body: Column(
+//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//           children: [
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               children: [
+//                 Container(
+//                   height: 500,
+//                   child: Column(
+//                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                     children: [
+//                       const Text(
+//                         'Team E',
+//                         style: TextStyle(
+//                           fontSize: 32,
+//                         ),
+//                       ),
+//                       Text(
+//                         '$teamAPoints',
+//                         style: const TextStyle(
+//                           fontSize: 150,
+//                         ),
+//                       ),
+//                       ElevatedButton(
+//                         style: ElevatedButton.styleFrom(
+//                           padding: const EdgeInsets.all(8),
+//                           backgroundColor: Colors.orange,
+//                           minimumSize: const Size(150, 50),
+//                         ),
+//                         onPressed: () {
+//                           setState(() {
+//                             teamAPoints++;
+//                           });
+//                           print(teamAPoints);
+//                         },
+//                         child: const Text(
+//                           'Add 1 Point ',
+//                           style: TextStyle(
+//                             fontSize: 18,
+//                             color: Colors.black,
+//                           ),
+//                         ),
+//                       ),
+//                       ElevatedButton(
+//                         style: ElevatedButton.styleFrom(
+//                           backgroundColor: Colors.orange,
+//                           minimumSize: const Size(150, 50),
+//                         ),
+//                         onPressed: () {
+//                           setState(() {
+//                             teamAPoints += 2;
+//                           });
+//                         },
+//                         child: const Text(
+//                           'Add 2 Point',
+//                           style: TextStyle(
+//                             fontSize: 18,
+//                             color: Colors.black,
+//                           ),
+//                         ),
+//                       ),
+//                       ElevatedButton(
+//                         style: ElevatedButton.styleFrom(
+//                           primary: Colors.orange,
+//                           minimumSize: const Size(150, 50),
+//                         ),
+//                         onPressed: () {
+//                           setState(() {
+//                             teamAPoints += 3;
+//                           });
+//                         },
+//                         child: const Text(
+//                           'Add 3 Point ',
+//                           style: TextStyle(
+//                             fontSize: 18,
+//                             color: Colors.black,
+//                           ),
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//                 Container(
+//                   height: 500,
+//                   child: const VerticalDivider(
+//                     indent: 50,
+//                     endIndent: 50,
+//                     color: Colors.grey,
+//                     thickness: 1,
+//                   ),
+//                 ),
+//                 Container(
+//                   height: 500,
+//                   child: Column(
+//                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                     children: [
+//                       const Text(
+//                         'Team B',
+//                         style: TextStyle(
+//                           fontSize: 32,
+//                         ),
+//                       ),
+//                       Text(
+//                         '$teamBPoints',
+//                         style: const TextStyle(
+//                           fontSize: 150,
+//                         ),
+//                       ),
+//                       ElevatedButton(
+//                         style: ElevatedButton.styleFrom(
+//                           padding: const EdgeInsets.all(8),
+//                           primary: Colors.orange,
+//                           minimumSize: const Size(150, 50),
+//                         ),
+//                         onPressed: () {
+//                           setState(() {});
+//                           teamBPoints++;
+//                         },
+//                         child: const Text(
+//                           'Add 1 Point ',
+//                           style: TextStyle(
+//                             fontSize: 18,
+//                             color: Colors.black,
+//                           ),
+//                         ),
+//                       ),
+//                       ElevatedButton(
+//                         style: ElevatedButton.styleFrom(
+//                           primary: Colors.orange,
+//                           minimumSize: const Size(150, 50),
+//                         ),
+//                         onPressed: () {
+//                           setState(() {});
+//                           teamBPoints += 2;
+//                         },
+//                         child: const Text(
+//                           'Add 2 Point ',
+//                           style: TextStyle(
+//                             fontSize: 18,
+//                             color: Colors.black,
+//                           ),
+//                         ),
+//                       ),
+//                       ElevatedButton(
+//                         style: ElevatedButton.styleFrom(
+//                           primary: Colors.orange,
+//                           minimumSize: const Size(150, 50),
+//                         ),
+//                         onPressed: () {
+//                           setState(() {
+//                             teamBPoints += 3;
+//                           });
+//                         },
+//                         child: const Text(
+//                           'Add 3 Point ',
+//                           style: TextStyle(
+//                             fontSize: 18,
+//                             color: Colors.black,
+//                           ),
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ],
+//             ),
+//             ElevatedButton(
+//               style: ElevatedButton.styleFrom(
+//                 padding: const EdgeInsets.all(8),
+//                 primary: Colors.orange,
+//                 minimumSize: const Size(150, 50),
+//               ),
+//               onPressed: () {
+//                 setState(() {
+//                   teamAPoints = 0;
+//                   teamBPoints = 0;
+//                 });
+//               },
+//               child: const Text(
+//                 'Reset',
+//                 style: TextStyle(
+//                   fontSize: 18,
+//                   color: Colors.black,
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
